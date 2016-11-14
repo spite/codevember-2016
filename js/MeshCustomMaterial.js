14088669f0104c423c26c28bf7ff202786b1458c
Jaume Sanchez <the.spite@gmail.com>
2016-11-14 04:30:55 +0100

    added lib

---
+++ js/MeshCustomMaterial.js
@@ -0,0 +1,40 @@
+function MeshCustomMaterial (parameters) {
+	THREE.MeshStandardMaterial.call( this );
+	this.uniforms = THREE.UniformsUtils.merge([
+		THREE.UniformsLib.common,
+		THREE.UniformsLib.aomap,
+		THREE.UniformsLib.lightmap,
+		THREE.UniformsLib.emissivemap,
+		THREE.UniformsLib.bumpmap,
+		THREE.UniformsLib.normalmap,
+		THREE.UniformsLib.displacementmap,
+		THREE.UniformsLib.roughnessmap,
+		THREE.UniformsLib.metalnessmap,
+		THREE.UniformsLib.fog,
+		THREE.UniformsLib.lights,
+		{
+			emissive : { value: new THREE.Color( 0x000000 ) },
+			roughness: { value: 0 },
+			metalness: { value: 1 },
+			envMapIntensity : { value: 1 },
+			time: { value: 0 }
+			}
+		]);
+
+	this.vertexShader = parameters.vertexShader;
+	this.fragmentShader = parameters.fragmentShader;
+	this.type = 'MeshCustomMaterial';
+
+	this.setValues(parameters);
+}
+
+MeshCustomMaterial.prototype = Object.create( THREE.MeshStandardMaterial.prototype );
+MeshCustomMaterial.prototype.constructor = MeshCustomMaterial;
+MeshCustomMaterial.prototype.isMeshStandardMaterial = true;
+
+MeshCustomMaterial.prototype.copy = function ( source ) {
+	THREE.MeshStandardMaterial.prototype.copy.call( this, source );
+	this.uniforms = THREE.UniformsUtils.clone(source.uniforms);
+	setFlags(this);
+	return this;
+};
