9bb6fdbb598bd09bdf31b178b87a8545f33e23b8
Jaume Sanchez <the.spite@gmail.com>
2016-11-20 22:09:43 +0100

    added fullscreen shortcut

--- js/Common.js
+++ js/Common.js
@@ -1,3 +1,12 @@
 document.getElementById( 'moreDetails' ).addEventListener( 'click', function( e ) {
 	document.getElementById( 'details' ).classList.toggle( 'hidden' );
 });
+
+function addFullscreenShortcut( element, callback ) {
+	window.addEventListener( 'keydown', function( e ) {
+		if( e.keyCode === 70 ) {
+			element.webkitRequestFullscreen();
+			callback();
+		}
+	} )
+}
