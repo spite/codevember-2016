1ea85c5c16bd6cd2c689b08fd16657ff1ac5b92d
Jaume Sanchez <the.spite@gmail.com>
2016-11-23 20:56:43 +0100

    added fullscren button

--- js/Common.js
+++ js/Common.js
@@ -3,10 +3,30 @@ document.getElementById( 'moreDetails' ).addEventListener( 'click', function( e 
 });
 
 function addFullscreenShortcut( element, callback ) {
+
+	function goFS() {
+
+		if(element.requestFullscreen) {
+			element.requestFullscreen();
+		} else if(element.mozRequestFullScreen) {
+			element.mozRequestFullScreen();
+		} else if(element.webkitRequestFullscreen) {
+			element.webkitRequestFullscreen();
+		} else if(element.msRequestFullscreen) {
+			element.msRequestFullscreen();
+		}
+		callback();
+
+	}
+
 	window.addEventListener( 'keydown', function( e ) {
 		if( e.keyCode === 70 ) {
-			element.webkitRequestFullscreen();
-			callback();
+			goFS();
 		}
 	} )
+	var btn = document.createElement( 'div' );
+	btn.textContent = '↗';
+	btn.style = 'position: absolute; right: 20px; bottom: 20px; border: 1px solid white; z-index: 1000; width: 28px; height: 28px; cursor: pointer; text-align: center; line-height: 24px';
+	document.body.appendChild( btn );
+	btn.addEventListener( 'click', goFS );
 }
